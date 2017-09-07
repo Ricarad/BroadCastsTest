12632c2dfebe376802727e8b32130027f2ca974f
Ricarad <15271013660@163.com>
2017-09-07 16:30:03 +0800

    Initial commit

---
+++ app/src/main/java/app/sensor/com/broadcaststest/MainActivity.java
@@ -0,0 +1,35 @@
+package app.sensor.com.broadcaststest;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private IntentFilter intentFilter;
+
+    private NetworkChangeReceiver networkChangeReceiver;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        intentFilter = new IntentFilter();
+        intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
+        networkChangeReceiver = new NetworkChangeReceiver();
+        registerReceiver(networkChangeReceiver,intentFilter);
+    }
+
+    protected void onDestroy(){
+        super.onDestroy();
+        unregisterReceiver(networkChangeReceiver);
+    }
+    class NetworkChangeReceiver extends BroadcastReceiver{
+        public void onReceive(Context context, Intent intent){
+            Toast.makeText(context,"newworke changes",Toast.LENGTH_SHORT).show();
+        }
+    }
+}
