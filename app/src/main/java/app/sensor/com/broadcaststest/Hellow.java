34af15f91963bd77e1fd9efe2d6338a672e379f7
Ricarad <15271013660@163.com>
2017-09-07 16:34:19 +0800

    增加hello类

---
+++ app/src/main/java/app/sensor/com/broadcaststest/Hellow.java
@@ -0,0 +1,12 @@
+package app.sensor.com.broadcaststest;
+
+/**
+ * Created by dongdong on 2017/9/7.
+ */
+
+public class Hellow {
+    private int sss;
+    public Hellow() {
+        this.sss = 1312;
+    }
+}
